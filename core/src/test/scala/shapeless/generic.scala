2ae7f87313914c78d43409ebc23c5654bb51fd58
Alexandre Archambault <alexandre.archambault@gmail.com>
2014-12-14 17:39:06 +0100

    Made FromTraversable more generic (also applies to generic products)

--- core/src/test/scala/shapeless/generic.scala
+++ core/src/test/scala/shapeless/generic.scala
@@ -555,4 +555,39 @@ class GenericTests {
     illTyped(" IsTuple[CP] ")
     illTyped(" IsCaseClass[CP] ")
   }
+  
+  @Test
+  def testFromTraversable {
+    import syntax.std.traversable._
+    
+    {
+      val l = List(1, "a")
+      val optCC = l.toGeneric[CC]
+      assertTypedEquals[Option[CC]](Option(CC(1, "a")), optCC)
+    }
+
+    {
+      val l = List(1, "a", true)
+      val optCC = l.toGeneric[CC]
+      assertTypedEquals[Option[CC]](None, optCC)
+    }
+
+    {
+      val l = Nil
+      val optCC = l.toGeneric[CC]
+      assertTypedEquals[Option[CC]](None, optCC)
+    }
+
+    {
+      val l = Nil
+      val optEmptyCC = l.toGeneric[EmptyCC]
+      assertTypedEquals[Option[EmptyCC]](Some(EmptyCC()), optEmptyCC)
+    }
+
+    {
+      val l = List("a")
+      val optEmptyCC = l.toGeneric[EmptyCC]
+      assertTypedEquals[Option[EmptyCC]](None, optEmptyCC)
+    }
+  }
 }
